9e52568a904f5e2b9c4f7afe0828de3eef96fac3
goodluckjjin <45454000+goodluckjjin@users.noreply.github.com>
2020-08-14 16:31:11 +0900

    Update view engine and template inheritance in lecture 02

--- 02_helloExpress/app.js
+++ 02_helloExpress/app.js
@@ -7,6 +7,16 @@ const contacts = require("./routes/contacts");
 const app = express();
 const port = 3000;
 
+//기본 세팅
+// autoescape: true => js가 그대로 노출됨, 사용자가 게시글을 작성할 떄 alert와 같이 tag 사용을 하여 사용자 공격을 할 수 있는 경우들을 무효화시키기 위해 태그 걸러줌 (false면 태그대로 허용)
+// autoescape: true인 상태에서 내가 뿌려줘야 하는 경우는
+// products {{ message }} 를 아래와 같이 변경한다.
+// products {{ message | safe }}
+nunjucks.configure("template", {
+  autoescape: true,
+  express: app,
+});
+
 app.get("/", (req, res) => {
   res.send("express start");
 });
--- 02_helloExpress/routes/admin.js
+++ 02_helloExpress/routes/admin.js
@@ -6,7 +6,13 @@ router.get("/", (req, res) => {
 });
 
 router.get("/products", (req, res) => {
-  res.send("admin products 이후 url");
+  // res.send("admin products 이후 url");
+  //app.js에세팅된 template 이후부터 파일 위치를 지정해준다.
+  res.render("admin/products.html", {
+    //products.html의 message에 뿌려준다
+    message: `<h1>태그가 출력됩니다.</h1>`,
+    online: "express",
+  });
 });
 module.exports = router;
 
